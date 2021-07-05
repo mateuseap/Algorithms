97faae1256facaf7b4ae58e8a87d9a1229a4efce
Mateus Elias <meap@cin.ufpe.br>
2021-07-05 02:03:32 -0300

    Atualização no código da BST
    Função size, preorder, inorder e posorder adicionadas. Falta terminar a questão clear da BST.

--- bst.c
+++ bst.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 
-typedef struct bstNode{
-    char element[10];
+typedef struct BSTNode{
     int key;
-    struct bstNode *left;
-    struct bstNode *right;
+    int element;
+    BSTNode *left;
+    BSTNode *right;
 } BSTNode;
 
 typedef struct{
@@ -14,65 +13,53 @@ typedef struct{
     int nodecount;
 } BST;
 
+BSTNode *create_bstnode(int k, int e){
+    BSTNode *n = (BSTNode*) malloc(sizeof(BSTNode));
+    n->key = k;
+    n->element = e;
+    n->left = n->right = NULL;
+    return n;
+}
+
 BST *create_bst(){
-    BST *tree;
-    tree = (BST*) malloc(sizeof(BST));
-    tree->root = NULL;
-    tree->nodecount = 0;
-    return tree;
+    BST *bst = (BST*) malloc(sizeof(BST));
+    bst->root = NULL;
+    bst->nodecount = 0;
+    return bst;
 }
 
-BSTNode *create_bstNode(char *e, int key){
-	BSTNode *node = (BSTNode *) malloc(1*sizeof(BSTNode));
-	node->left = NULL;
-	node->right = NULL; 
-	node->key = key;
-	strcpy(node->element, e);
-	return node;
-}
-
-int size(BST *bst){
-    return bst->nodecount;
-}
-
-char *findhelp(BSTNode *rt, int key, char x[]){
+int findhelp(BSTNode *rt, int k){
     if(rt == NULL){
-        return NULL;
+        return 0;
     }
-    if(rt->key > key){
-        return findhelp(rt->left, key, x);
-    }else if(rt->key == key){
+    if(rt->key > k){
+        return findhelp(rt->left, k);
+    }else if(rt->key == k){
         return rt->element;
     }else{
-        return findhelp(rt->right, key, x);
+        return findhelp(rt->right, k);
     }
 }
 
-char *find(BST *bst, int key, char x[]){
-    return findhelp(bst->root, key, x);
+int find(BST *bst, int k){
+    return findhelp(bst->root, k);
 }
 
-BSTNode *inserthelp(BSTNode *rt, int key, char x[]){
+BSTNode *inserthelp(BSTNode *rt, int k, int e){
     if(rt == NULL){
-        return create_bstNode(x, key);
-    }else if(rt->key > key){
-        rt->left = inserthelp(rt->left, key, x);
+        return create_bstnode(k, e);
+    }
+    if(rt->key > k){
+        rt->left = inserthelp(rt->left, k, e);
     }else{
-        rt->right = inserthelp(rt->right, key, x);
+        rt->right = inserthelp(rt->right, k, e);
     }
     return rt;
 }
 
-void insert(BST *bst, int key, char x[]){
-    bst->root = inserthelp(bst->root, key, x);
+void insert(BST *bst, int k, int e){
+    bst->root = inserthelp(bst->root, k, e);
     bst->nodecount++;
-}
-
-BSTNode *getmin(BSTNode *rt){
-    if(rt->left == NULL){
-        return rt;
-    }
-    return getmin(rt->left);
 }
 
 BSTNode *deletemin(BSTNode *rt){
@@ -83,49 +70,75 @@ BSTNode *deletemin(BSTNode *rt){
     return rt;
 }
 
-BSTNode *removehelp(BSTNode *rt, int key, char x[]){
+BSTNode *getmin(BSTNode *rt){
+    if(rt->left == NULL){
+        return rt;
+    }
+    return getmin(rt->left);
+}
+
+BSTNode *removehelp(BSTNode *rt, int k){
     if(rt == NULL){
         return NULL;
     }
-    if(rt->key > key){
-        rt->left = removehelp(rt->left, key, x);
-    }else if(rt->key < key){
-        rt->right = removehelp(rt->right, key, x);
+    if(rt->key > k){
+        rt->left = removehelp(rt->left, k);
+    }else if(rt->key < k){
+        rt->right = removehelp(rt->right, k);
     }else{
         if(rt->left == NULL){
-            BSTNode *aux = rt->right;
-            free(rt);
-            return aux;
+            return rt->right;
         }else if(rt->right == NULL){
-            BSTNode *aux = rt->left;
-            free(rt);
-            return aux;
+            return rt->left;
         }else{
-            BSTNode *temp;
-            //temp = (BSTNode*) malloc(sizeof(BSTNode));
-            temp = getmin(rt->right);
-            strcpy(rt->element, temp->element);
+            BSTNode *temp = getmin(rt->right);
+            rt->element = temp->element;
             rt->key = temp->key;
             rt->right = deletemin(rt->right);
+            free(temp);
         }
     }
+    return rt;
 }
 
-char *remover(BST *bst, int key, char x[]){
-    char *temp;
-    temp = findhelp(bst->root, key, x);
-    if(temp != NULL){
-        bst->root = removehelp(bst->root, key, x);
+int remove(BST *bst, int k){
+    int temp = findhelp(bst->root, k);
+    if(temp != 0){
+        bst->root = removehelp(bst->root, k);
         bst->nodecount--;
     }
     return temp;
 }
 
-void visit_pos(BSTNode *rt){
-    if(rt != NULL){
-		visit_pos(rt->left);
-		visit_pos(rt->right);
-		free(rt);
+int size(BST *bst){
+    return bst->nodecount;
+}
+
+void clear(BST *bst){
+    
+}
+
+void preorder(BSTNode *rt){
+    if(rt != 0){
+        printf("%d\n", rt->key);
+        preorder(rt->left);
+        preorder(rt->right);
+    }
+}
+
+void inorder(BSTNode *rt){
+    if(rt != 0){
+        inorder(rt->left);
+        printf("%d\n", rt->key);
+        inorder(rt->right);
+    }
+}
+
+void posorder(BSTNode *rt){
+    if(rt != 0){
+        posorder(rt->left);
+        posorder(rt->right);
+        printf("%d\n", rt->key);
     }
 }
 
