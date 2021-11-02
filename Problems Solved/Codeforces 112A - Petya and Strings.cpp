13a84fd2909a71027460a08fdb2767a08b252155
Mateus Elias <meap@cin.ufpe.br>
2021-11-02 08:16:49 -0300

    Create Codeforces 112A - Petya and Strings.cpp

---
+++ Problems Solved/Codeforces 112A - Petya and Strings.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <strings.h>
+#include <string.h>
+
+using namespace std;
+
+int main(){
+    string st1, st2;
+    char str1[101], str2[101];
+
+    scanf(" %[^\n]", str1);
+    scanf(" %[^\n]", str2);
+
+    if(strcasecmp(str1,str2) == 0){
+        printf("0");
+    }else{
+        for(int i = 0; i < strlen(str1); i++){
+            str1[i] = tolower(str1[i]);
+            str2[i] = tolower(str2[i]);
+        }
+
+        st1 = str1;
+        st2 = str2;
+
+        if(st1 < st2){
+            printf("-1");
+        }else{
+            printf("1");
+        }
+    }
+
+    return 0;
+}
