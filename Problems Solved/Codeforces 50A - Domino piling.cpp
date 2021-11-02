74a2b1f8f0b279b8a9fca315a4853900fff3f89c
Mateus Elias <meap@cin.ufpe.br>
2021-11-02 08:17:01 -0300

    Create Codeforces 50A - Domino piling.cpp

---
+++ Problems Solved/Codeforces 50A - Domino piling.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int M, N;
+
+    cin>>M>>N;
+
+    cout<<floor(M*N*0.5);
+
+    return 0;
+}
