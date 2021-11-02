b72fa21e064c60a9870aca2b2d0d33de004afe06
Mateus Elias <meap@cin.ufpe.br>
2021-11-02 08:16:52 -0300

    Create Codeforces 158A - Next Round.cpp

---
+++ Problems Solved/Codeforces 158A - Next Round.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, k, ans = 0;
+    vector<int> scores;
+
+    cin>>n>>k;
+
+    for(int i = 0; i < n; i++){
+        cin>>scores[i];
+    }
+    
+    for(int i = n-1; i >= 0; i--){
+        if(scores[i] >= scores[k-1] && scores[i] > 0){
+            ans++;
+        }
+    }
+    cout<<ans;
+
+    return 0;
+}
